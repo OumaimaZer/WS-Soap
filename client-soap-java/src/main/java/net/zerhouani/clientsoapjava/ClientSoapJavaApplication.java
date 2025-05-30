c271be34543b8a4b2065961e3495047d53e8ef34
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-30 19:59:43 +0100

    Adding Modul client-soap-java

---
+++ client-soap-java/src/main/java/net/zerhouani/clientsoapjava/ClientSoapJavaApplication.java
@@ -0,0 +1,13 @@
+package net.zerhouani.clientsoapjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @SpringBootApplication
+public class ClientSoapJavaApplication {
+
+    public static void main(String[] args) {
+        S pringApplication.run(ClientSoapJavaApplication.class, args);
+    }
+
+}**/
---
+++ client-soap-java/src/test/java/net/zerhouani/clientsoapjava/ClientSoapJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zerhouani.clientsoapjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSoapJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
